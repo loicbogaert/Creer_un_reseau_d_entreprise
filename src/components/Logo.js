611bbc49a7037397d510dea4101fc38779646ffa
Loïc Bogaert <loicbogaertvauban@gmail.com>
2021-06-17 14:40:13 +0200

    form labels made easier to read, mobile format added

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,7 @@ const Logo = () => {
         <div className="logo">
             <img src="./img/logo-black.svg" alt="logo de Groupomania"></img>
             <p className="welcome">Dear employee, welcome to Groupomania's chatting app reserved to all of our employees. 
-            In order to start chatting with everyone, you must create an account and then log in (you will find those features on the top-right corner of the app).
+            In order to start chatting with everyone, you must create an account and then log in (you will find those features on the top-right corner of the app, mid-top for mobiles).
             <br></br><br></br>
             We sincerely hope you will enjoy our app, sharing content and chatting with your co-workers.
             </p>
